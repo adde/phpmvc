3ea99dcb9c05b1d8f77e121f9348fd5118d508d9
Andreas Jönsson <adde90@gmail.com>
2014-10-01 08:41:02 +0200

    Adding index in the comment template.

--- app/view/comment/comments.tpl.php
+++ app/view/comment/comments.tpl.php
@@ -6,7 +6,7 @@
   <div class='comments'>
     <?php foreach ($comments as $id => $comment) : ?>
       <div class="comment">
-        <h3><a href="<?=$comment['web']?>"><?=$comment['name']?></a> - <small><?=date('Y-m-d H:i:s', $comment['timestamp'])?></small></h3>
+        <h3>#<?=$id+1?> <a href="<?=$comment['web']?>"><?=$comment['name']?></a> - <small><?=date('Y-m-d H:i:s', $comment['timestamp'])?></small></h3>
         <p><?=$comment['content']?></p>
         <a class="btn-edit" title="Edit comment" href="<?=$this->url->create('')?>?id=<?=$id?>&amp;page=<?=$this->request->getCurrentUrl()?>#comments"><i class="fa fa-edit"></i></a>
         <a class="btn-delete" title="Delete comment" href="<?=$this->url->create('comment/remove?id=' . $id)?>&amp;page=<?=$this->request->getCurrentUrl()?>"><i class="fa fa-times"></i></a>
