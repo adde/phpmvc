a3db02733a1c7d52d0382dfb9feb4f81b65f5f95
Andreas Jönsson <adde90@gmail.com>
2014-10-01 21:41:36 +0200

    Changing edit link to current url instead of root.

--- app/view/comment/comments.tpl.php
+++ app/view/comment/comments.tpl.php
@@ -8,7 +8,7 @@
       <div class="comment">
         <h3>#<?=$id+1?> <a href="<?=$comment['web']?>"><?=$comment['name']?></a> - <small><?=date('Y-m-d H:i:s', $comment['timestamp'])?></small></h3>
         <p><?=$comment['content']?></p>
-        <a class="btn-edit" title="Edit comment" href="<?=$this->url->create('')?>?id=<?=$id?>&amp;page=<?=$this->request->getCurrentUrl()?>#comments"><i class="fa fa-edit"></i></a>
+        <a class="btn-edit" title="Edit comment" href="<?=$this->request->getCurrentUrl()?>?id=<?=$id?>&amp;page=<?=$this->request->getCurrentUrl()?>#comments"><i class="fa fa-edit"></i></a>
         <a class="btn-delete" title="Delete comment" href="<?=$this->url->create('comment/remove?id=' . $id)?>&amp;page=<?=$this->request->getCurrentUrl()?>"><i class="fa fa-times"></i></a>
       </div>
     <?php endforeach; ?>
